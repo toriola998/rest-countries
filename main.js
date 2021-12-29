d93662f100624d19c2ce5102822abd1a8206868a
Toriola Faidat <faaedah@gmail.com>
2021-12-29 21:56:23 +0100

    added render countries function

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const wrapper = document.querySelector('.wrapper')
-const searchCountry = document.querySelector('#input-country')
-let html = "";
-let results = ""
+//const searchCountry = document.querySelector('#input-country')
+//let html = "";
+//let results = ""
 
 fetchCountries();
+
 function fetchCountries () {
     fetch('https://restcountries.com/v2/all')
     .then(function (response) {
@@ -11,6 +12,24 @@ function fetchCountries () {
     })
     .then(function (data) {
         console.log(data)
-        //renderCountries(data);
+        renderCountries(data);
     })
 }
+
+function renderCountries (data) {
+    data.forEach( (country) => {
+       wrapper.innerHTML += `
+        <div class="container">
+            <div class="box"> 
+               <img src="${country.flags.svg}">
+               <div class="details">  
+                   <h3>${country.name}</h3>
+                   <p><span>Population:</span>${country.population}</p>
+                   <p><span>Region:</span>${country.region}</p>
+                   <p><span>Capital:</span>${country.capital}</p>
+               </div>
+           </div>
+        </div>`      
+    }) 
+}
+
